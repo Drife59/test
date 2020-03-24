2db26080b7f1bbd8003dc8a54153522ad3931533
benjamin.grassart <benjamin.grassart@thalesgroup.com>
2020-03-24 15:32:53 +0100

    Toggle display User Home

--- test-react/memoracarContext/UserContext/src/user/Home.js
+++ test-react/memoracarContext/UserContext/src/user/Home.js
@@ -26,21 +26,22 @@ class UserHome extends Component {
         return (
             <UserContext.Consumer>
             {value => 
-            <div id="homeUser" style={{display: this.state.showUserHome ? 'block' : 'none' }} >
+            <div id="homeUser" >
                     <h1 onClick={this.toggleUserDisplay}> user context section </h1>
+                    <div style={{display: this.state.showUserHome ? 'block' : 'none' }} >
+                        <table >
+                            <tbody>
+                            <tr><td>User id:</td><td>{value.userid}</td></tr>
+                            <tr><td>User email:</td><td>{value.email}</td></tr>
+                            <tr><td>User token:</td><td>{value.token}</td></tr>
+                            <tr><td>User role:</td><td>{value.role}</td></tr>
+                            </tbody>
+                        </table>
 
-                    <table >
-                        <tbody>
-                        <tr><td>User id:</td><td>{value.userid}</td></tr>
-                        <tr><td>User email:</td><td>{value.email}</td></tr>
-                        <tr><td>User token:</td><td>{value.token}</td></tr>
-                        <tr><td>User role:</td><td>{value.role}</td></tr>
-                        </tbody>
-                    </table>
-
-                    <FormAuthent />
-                    <UpdatePassword/>
-                    <LoadSession />
+                        <FormAuthent />
+                        <UpdatePassword/>
+                        <LoadSession />
+                    </div>
             </div>
             }
             </UserContext.Consumer>
