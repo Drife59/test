3328ec698eb618787a2b702addc82c579b8179fc
benjamin.grassart <benjamin.grassart@thalesgroup.com>
2020-03-24 15:29:28 +0100

    Delete useless code from App component

--- test-react/memoracarContext/UserContext/src/App.js
+++ test-react/memoracarContext/UserContext/src/App.js
@@ -2,47 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-import { UserContext } from "./store/UserProvider";
 import UserHome from './user/Home';
 
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {userLoggedIn: false};
-
-        this.updateUserLoggedIn = this.updateUserLoggedIn.bind(this);
-
-    }
-
-    componentDidMount(){
-        let value = this.context;
-
-        this.setState({
-            userLoggedIn: value.isUserLoggedIn()
-        });
-    }
-
-    updateUserLoggedIn(status){
-        this.setState({
-            userLoggedIn: status
-        })
     }
 
     render() {
         
         return (
-            <UserContext.Consumer>
-            {value => 
             <div className="App" >
                 <UserHome />
             </div>
-            }
-            </UserContext.Consumer>
         );
     }
 }
-
-App.contextType = UserContext; 
 
 export default App;
